ba4ec56acd494666bc8ea6778f64cca2de7c117b
bedolaga22809 <ebz.m.d@mail.ru>
2025-10-31 19:51:22 +0000

    completed point 1

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 #include <string>
 
 int main() {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
+
+	std::cout << "$ ";
+
+	//печатаем введенную строку и выводим
+	std::string a;
+	std::cin >> a;
+	std::cout << "введенная строка: " << a << std::endl;
 }
